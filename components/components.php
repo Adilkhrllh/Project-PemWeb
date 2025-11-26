b8558f1c2f1550020a245bd8a0436c1366aaab42
Adilkhrllh <fhardani5@gmail.com>
2025-11-26 22:08:22 +0700

    Update components.php

--- components/components.php
+++ components/components.php
@@ -1,32 +1,13 @@
+<!-- COMPONENTS.PHP -->
 <?php
 function head($title)
 { ?>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= $title ?></title>
-  <!-- CSS Boostrap -->
-  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous"> -->
-  <!-- CSS Font Awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
-  <!-- CSS Custom -->
   <link rel="stylesheet" href="css/style.css">
 <?php } ?>
-
-<?php
-function footer()
-{ ?>
-  <!-- JS Bootstrap -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-  <!-- Untuk UX menghapus status urlnya || Silahkan uncomment kalo mau make, gak usah biarain juga boleh -->
-  <!-- <script>
-    // Hapus ?status=... dari URL setelah toast tampil
-    if (window.location.search.includes("status=")) {
-      window.history.replaceState({}, document.title, window.location.pathname);
-    }
-  </script> -->
-<?php
-}
-?>
 
 <?php
 function listAlert($status)
@@ -84,13 +65,9 @@ function navbar()
       <a class="navbar-brand fw-bold fs-3 text-white" href="{kode kamu}">
         To Do List Saya
       </a>
-      <a href="logic/logout.php"><button class="btn btn-danger">Logout</button></a>
+      <a href="../logic/logout.php"><button class="btn btn-danger">Logout</button></a>
     </div>
   </nav>
 <?php } ?>
 
 <?php
-function formatDate($date)
-{
-  return date('d F Y', strtotime($date));
-}?>
