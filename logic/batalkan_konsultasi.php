d75287c8240bad4e204dfeebe509663d8bf75a00
Adilkhrllh <fhardani5@gmail.com>
2025-11-27 12:13:25 +0700

    Delete logic directory

--- logic/batalkan_konsultasi.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-include("../config/koneksi.php");
-if(isset($_GET['id'])){
-    $id = $_GET['id'];
-    mysqli_query($koneksi, "UPDATE konsultasi SET status_konsul='dibatalkan' WHERE id_konsultasi=$id AND id_pasien=".$_SESSION['id_user']);
-    header("Location: ../pasien/riwayat_konsultasi.php");
-}
-?>
