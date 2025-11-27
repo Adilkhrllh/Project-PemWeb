5dfa34917f706d5db3329f38f9d2483139bde7cb
Adilkhrllh <fhardani5@gmail.com>
2025-11-27 12:12:40 +0700

    Delete config directory

--- config/koneksi.php
+++
@@ -1,9 +0,0 @@
-<!-- KONEKSI.PHP -->
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$database = "klinik_bantul";
-
-$koneksi = mysqli_connect($host, $user, $pass, $database) or die("koneksi gagal");
-?>
